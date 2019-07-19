70311196f313199fc57420890d7656f78aacb340
Sophia Wagner <sophia.wagner@t-online.de>
2019-07-19 10:26:52 +0200

    deleated HEAD

--- lib/config.py
+++ lib/config.py
@@ -4,15 +4,9 @@ from easydict import EasyDict
 CONF = EasyDict()
 
 # BASE PATH
-<<<<<<< HEAD
-CONF.ROOT = "/home/lorenzlamm/Dokumente/DavesPointnetClone/Pointnet2.ScanNet" # TODO change this
-CONF.SCANNET_DIR =  "/home/lorenzlamm/Downloads/scans/scans" # TODO change this
-CONF.SCENE_NAMES = os.listdir('/home/lorenzlamm/Downloads/scans/scans') # TODO change
-=======
 CONF.ROOT = "/workspace/pointnet_train/Pointnet2.ScanNet" # TODO change this
 CONF.SCANNET_DIR =  "/mnt/canis/Datasets/ScanNet/public/v2/scans" # TODO change this
 #CONF.SCENE_NAMES = os.listdir('/mnt/canis/Datasets/ScanNet/public/v1/scans') # TODO change
->>>>>>> 492875351ce329ba901829f996103c7bd052e9be
 
 CONF.PREP = os.path.join(CONF.ROOT, "preprocessing")
 CONF.PREP_SCANS = os.path.join(CONF.PREP, "scannet_scenes")
